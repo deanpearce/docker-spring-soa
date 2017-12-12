7f94c9440428156e3935dc22647f2c7ade0d517c
Dean Pearce <pearce@pythian.com>
2017-12-11 20:05:01 -0500

    Generated basic Spring application, added basic Docker support.

---
+++ src/main/java/com/budget/core/BudgetbitSoaCoreApplication.java
@@ -0,0 +1,12 @@
+package com.budget.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbitSoaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbitSoaCoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/budget/core/BudgetbitSoaCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.budget.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetbitSoaCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
